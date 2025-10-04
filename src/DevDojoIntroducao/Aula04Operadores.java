79b4b5a59d6f78a656ac977b136f8ba62210608a
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-10-04 00:12:51 -0300

    Aula4Operadores

--- src/DevDojoIntroducao/Aula04Operadores.java
+++ src/DevDojoIntroducao/Aula04Operadores.java
@@ -54,6 +54,28 @@ public class Aula04Operadores {
 
 
 
+        // = += -= /= %=  - Atribuição
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; //1800
+        bonus *= 2; //3600
+        bonus /= 2; //1800
+        bonus %= 2; // 0
+        System.out.println(bonus);
+
+        // ++ --
+
+        int contador = 0;
+        contador += 1; //Contador = Contador + 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
+
+
+
 
     }
 }
