de8319c71f9aeaefe1f5368a18c151da6a737e7f
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-10-08 00:48:26 -0300

    Aula05EstruturaDeRepeticao05

---
+++ src/DevDojoIntroducao/Aula05EstruturasDeRepeticao05.java
@@ -0,0 +1,47 @@
+package DevDojoIntroducao;
+
+public class Aula05EstruturasDeRepeticao05 {
+    public static void main(String[] args) {
+        // Imprima o dia da semana, considerando 1 como domingo
+        byte dia = 10;
+
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Seg");
+                break;
+            case 3:
+                System.out.println("Ter");
+                break;
+            case 4:
+                System.out.println("Qua");
+                break;
+            case 5:
+                System.out.println("Qui");
+                break;
+            case 6:
+                System.out.println("Sex");
+                break;
+            case 7:
+                System.out.println("Sab");
+                break;
+            default:
+                System.out.println("Opcao invalida");
+                break;
+        }
+
+        char sexo = 'M';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+            default:
+                System.out.println("Opcao invalida");
+        }
+    }
+}
