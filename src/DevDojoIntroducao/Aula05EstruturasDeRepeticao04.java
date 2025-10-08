c27f52d81c0edf1ff669b57b521b8ccb27a67ec2
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-10-08 00:11:56 -0300

    Aula05EstruturasDeRepeticao04

---
+++ src/DevDojoIntroducao/Aula05EstruturasDeRepeticao04.java
@@ -0,0 +1,19 @@
+package DevDojoIntroducao;
+
+public class Aula05EstruturasDeRepeticao04 {
+    public static void main(String[] args) {
+          double salarioAnual = 70000;
+          double primeiraFaixa = 9.70 / 100;
+          double segundaFaixa = 37.35 / 100;
+          double terceiraFaixa = 49.50 / 100;
+          double valorImposto;
+          if (salarioAnual <= 34712) {
+              valorImposto = salarioAnual * primeiraFaixa;
+          } else if (salarioAnual >= 34713 && salarioAnual <= 68057) {
+              valorImposto = salarioAnual * segundaFaixa;
+          } else {
+              valorImposto = salarioAnual * terceiraFaixa;
+          }
+        System.out.println("O valor do Imposto a ser pago e de : " + valorImposto);
+    }
+}
