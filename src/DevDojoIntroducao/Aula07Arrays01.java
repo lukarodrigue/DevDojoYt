2fe623710596dae6351569862ede56d7441eff23
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-10-13 00:12:36 -0300

    Aula07Arrays02

--- src/DevDojoIntroducao/Aula07Arrays01.java
+++ src/DevDojoIntroducao/Aula07Arrays01.java
@@ -3,9 +3,9 @@ package DevDojoIntroducao;
 public class Aula07Arrays01 {
     public static void main(String[] args) {
 
-        // Estrutura int[] -> define o tipo do array (neste caso, um array de inteiros)
-        // idades -> nome da variável (referência ao array)
-        // new int[quantidadeDePosicoes] -> cria o objeto array alocando espaço na memória
+        //Estrutura int[] - define o tipo do array
+        //idades - nome da variavel
+        //new int[quantidade de espaco reservado na memoria]; - cria o objeto array alocando os espacos na memoria
 
         int[] idades = new int[3];
         idades[0] = 21;
---
+++ src/DevDojoIntroducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package DevDojoIntroducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float e double = 0
+        //char - '\u0000' 'espaço vazio'
+        //boolean - false
+        //String - null
+        //OBS TODOS ESSES VALORES SAO QUANDO AS VARIAVEIS NAO SAO INICIALIZADAS
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
