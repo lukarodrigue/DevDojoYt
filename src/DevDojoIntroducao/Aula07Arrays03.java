60d401c35e54a1e8f71b67c1c71e2ab109b45bf0
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-10-15 22:59:18 -0300

    Aula07Arrays03 Refactor

--- src/DevDojoIntroducao/Aula07Arrays03.java
+++ src/DevDojoIntroducao/Aula07Arrays03.java
@@ -10,9 +10,12 @@ public class Aula07Arrays03 {
         //Outra forma de Declarar o Arrays
         int[] numeros3 = new int[]{1, 2, 3, 4, 5};
 
-        for (int i = 0; i < numeros2.length; i++) {
-            System.out.println(numeros2[i]);
-
+//        for (int i = 0; i < numeros2.length; i++) {
+//            System.out.println(numeros2[i]);
+//
+//        }
+        for (int num : numeros3) {
+            System.out.println(num);
         }
     }
 }
