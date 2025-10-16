86d542fcc214b9c0e33e7214b72799e7e0d80439
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-10-15 23:00:10 -0300

    Aula08ArraysMultidimensionais01

---
+++ src/DevDojoIntroducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,25 @@
+package DevDojoIntroducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+        for (int i = 0; i < dias.length ; i++) {
+            for (int j = 0; j < dias[i].length ; j++) {
+                System.out.println(dias[i][j]);
+            }
+            System.out.println("--------------------------------------------");
+        }
+        for (int [] arryBase : dias) {
+            for (int num : arryBase) {
+                System.out.println(num);
+            }
+        }
+    }
+}
