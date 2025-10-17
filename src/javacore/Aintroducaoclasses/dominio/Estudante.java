4cb0205b97bdf7b2f117d5f955ecb0833e21d98d
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-10-17 00:10:19 -0300

    OrientacaoAObjetos01

--- src/javacore/Aintroducaoclasses/dominio/Estudante.java
+++ src/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -1,7 +1,7 @@
 package javacore.Aintroducaoclasses.dominio;
 
 public class Estudante {
-    public String nome;
+    public String nome = "jailson";
     public int idade;
     public char sexo;
 }
---
+++ src/javacore/Aintroducaoclasses/test/EstudanteTest02.java
@@ -0,0 +1,23 @@
+package javacore.Aintroducaoclasses.test;
+
+import javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        Estudante estudante2 = new Estudante();
+
+        estudante.nome = "roberto";
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.idade);
+
+        System.out.println("---------------------");
+
+
+        System.out.println(estudante2.nome);
+        System.out.println(estudante2.sexo);
+        System.out.println(estudante2.idade);
+    }
+}
