6d721ac1058e3392da899501053303773f63ef91
luka <lukarodrigues25@gmail.com>
2025-10-20 10:42:03 -0400

    OrientacaoAObjetosMetodos

---
+++ src/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,11 @@
+package javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros(){
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(20 - 10);
+    }
+}
---
+++ src/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,13 @@
+package javacore.Bintroducaometodos.test;
+
+import javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("--------------------");
+        calculadora.subtraiDoisNumeros();
+
+    }
+}
