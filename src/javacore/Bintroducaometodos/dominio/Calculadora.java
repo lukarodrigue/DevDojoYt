4e54da1025fa966bfbe96320c667c7093cc97b24
luka <lukarodrigues25@gmail.com>
2025-11-04 22:54:47 -0300

    OrientacaoAObjetosMetodos02

--- src/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -8,4 +8,10 @@ public class Calculadora {
     public void subtraiDoisNumeros() {
         System.out.println(20 - 10);
     }
+
+    public void multiplicaDoisNumeros(int num1, float num2) {
+        System.out.println(num1 * num2);
+    }
+
+
 }
