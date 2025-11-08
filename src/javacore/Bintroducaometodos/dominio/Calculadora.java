8459f469095a16a6b47f92d04ad72c5872869c64
luka <lukarodrigues25@gmail.com>
2025-11-07 23:56:48 -0300

    OrientacaoAObjetosMetodosRetorno04

--- src/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -19,4 +19,25 @@ public class Calculadora {
         }
         return num1 / num2;
     }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        } else
+            return num1 / num2;
+        }
+
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Nao existe divisao por 0");
+            return;
+        }
+            System.out.println(num1/num2);
+
+    }
+
 }
+
+
+
---
+++ src/javacore/Bintroducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,7 @@
+//package javacore.Bintroducaometodos.test;
+//
+//public class CalculadoraTest04 {
+//    public static void main(String[] args) {
+//
+//    }
+//}
