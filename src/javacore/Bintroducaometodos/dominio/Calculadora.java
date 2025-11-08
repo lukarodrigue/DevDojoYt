d4d2d0d2060b04d6ad404ce67eefc6d70ba1192a
luka <lukarodrigues25@gmail.com>
2025-11-08 00:10:49 -0300

    OrientacaoAObjetosMetodosParametros05

--- src/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -37,6 +37,14 @@ public class Calculadora {
 
     }
 
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do altera dois numeros");
+        System.out.println("Num1 "+num1);
+        System.out.println("Num2 "+num2);
+    }
+
 }
 
 
